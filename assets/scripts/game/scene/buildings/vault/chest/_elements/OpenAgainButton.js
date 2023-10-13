39072194d67528e5d0e7505dbbd9c23ecc7f6a68
Eryczal <79263592+Eryczal@users.noreply.github.com>
2023-10-13 14:00:10 +0200

    Enhancements to chest opening functionality
    Changed rarity to integer in card data.
    
    Chests now generate card as a reward.
    
    Added open again button.

---
+++ assets/scripts/game/scene/buildings/vault/chest/_elements/OpenAgainButton.js
@@ -0,0 +1,38 @@
+import { BuyButton } from "../../../../_elements/BuyButton.js";
+import { chests } from "../../../../../data/chests.js";
+
+class OpenAgainButton extends BuyButton {
+	constructor(game, x, y, width, height, parent) {
+		super(game, x, y, width, height, parent);
+
+		this.clickable = this.canBuy();
+
+		if (this.canBuy()) {
+			this.color = "#999";
+		} else {
+			this.color = "#000";
+		}
+
+		this.text = "Otwórz ponownie";
+	}
+
+	onClick(mouseX, mouseY) {
+		if (this.isMouseOver(mouseX, mouseY) && this.canBuy()) {
+			this.game.playerManager.coin -= chests[this.parent.chestId].cost;
+
+			this.parent.openAgain();
+		}
+	}
+
+	onRightClick(mouseX, mouseY) {
+		if (this.isMouseOver(mouseX, mouseY) && this.game.playerManager.gem === "max") {
+			this.parent.openAgain();
+		}
+	}
+
+	canBuy() {
+		return this.game.playerManager.coin >= chests[this.parent.chestId].cost;
+	}
+}
+
+export { OpenAgainButton };
--- assets/scripts/game/scene/buildings/vault/chest/_elements/OpeningChest.js
+++ assets/scripts/game/scene/buildings/vault/chest/_elements/OpeningChest.js
@@ -1,5 +1,7 @@
 import { Element } from "../../../../../element/Element.js";
 import { chests } from "../../../../../data/chests.js";
+import { cards } from "../../../../../data/cards.js";
+import { OpenAgainButton } from "./OpenAgainButton.js";
 
 class OpeningChest extends Element {
 	constructor(game, menu) {
@@ -21,6 +23,8 @@ class OpeningChest extends Element {
 		this.chestId = this.game.sceneManager.currentScene.data.id;
 		this.opened = false;
 		this.openingAnimation = false;
+
+		this.openAgainButton;
 	}
 
 	openChest() {
@@ -29,23 +33,33 @@ class OpeningChest extends Element {
 		}
 		this.y += this.ICON_SIZE / 50;
 
-		if (this.y >= this.iY + this.ICON_SIZE) {
+		if (this.y >= this.iY + this.ICON_SIZE / 2) {
 			this.headerOpacity = 0;
-			this.y = this.iY + this.ICON_SIZE;
+			this.y = this.iY + this.ICON_SIZE / 2;
 			this.endAnimation();
 		} else {
 			setTimeout(() => this.openChest(), 16);
 		}
 	}
 
+	openAgain() {
+		this.opened = false;
+		this.headerOpacity = 1;
+		this.y = this.iY;
+	}
+
 	endAnimation() {
 		this.openingAnimation = false;
 		this.opened = true;
+		this.openAgainButton = new OpenAgainButton(this.game, this.x, this.y + this.ICON_SIZE, this.ICON_SIZE * 2, this.ICON_SIZE / 4, this);
 	}
 
 	generateDrop() {
 		let chance = Math.floor(Math.random() * 1000);
 		let rarity = chests[this.chestId].chances.findIndex((threshold) => chance < threshold);
+		let array = cards.filter((card) => card.rarity === rarity);
+		let index = Math.floor(Math.random() * array.length);
+		console.log(array[index]);
 	}
 
 	draw() {
@@ -53,15 +67,27 @@ class OpeningChest extends Element {
 
 		if (this.opened === false) {
 			this.game.writeText("Otwórz", this.HEADER_X, this.HEADER_Y, this.HEADER_SIZE, `rgba(0, 0, 0, ${this.headerOpacity})`);
+		} else {
+			this.openAgainButton.draw();
+			// this.game.ctx.drawImage(this.game.assetsManager.images.buyButton, this.x, this.y + this.ICON_SIZE, this.ICON_SIZE * 2, this.ICON_SIZE / 4);
+			// this.game.writeText("Otwórz ponownie", this.x + this.ICON_SIZE / 2, this.y + this.ICON_SIZE + this.ICON_SIZE / 8, this.ICON_SIZE / 4);
 		}
 		this.game.ctx.drawImage(this.game.assetsManager.images[chests[this.chestId].image + "Chest"], this.x, this.y, this.ICON_SIZE, this.ICON_SIZE);
 	}
 
 	onClick(mouseX, mouseY) {
-		if (this.openingAnimation === false) {
+		if (this.openingAnimation === false && this.opened === false) {
 			this.generateDrop();
 			this.openingAnimation = true;
 			this.openChest();
+		} else {
+			this.openAgainButton.onClick(mouseX, mouseY);
+		}
+	}
+
+	onRightClick(mouseX, mouseY) {
+		if (this.openingAnimation === false && this.opened === true) {
+			this.openAgainButton.onRightClick(mouseX, mouseY);
 		}
 	}
 
