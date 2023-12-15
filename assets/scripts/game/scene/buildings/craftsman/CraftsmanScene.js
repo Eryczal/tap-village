32b500357a19385092ec83a81e6157d9f8f43919
Eryczal <79263592+Eryczal@users.noreply.github.com>
2023-12-15 13:20:37 +0100

    Fix Craftsman onResize

--- assets/scripts/game/scene/buildings/craftsman/CraftsmanScene.js
+++ assets/scripts/game/scene/buildings/craftsman/CraftsmanScene.js
@@ -7,12 +7,21 @@ class CraftsmanScene extends BuildingScene {
     }
 
     initChild(menu) {
+        this.menu = menu;
         this.elementsHolder.addElement("ChangePositionButton", new ChangePositionButton(this.game, menu));
         this.elementsHolder.addElement("RemoveObjectButton", new RemoveObjectButton(this.game, menu));
     }
 
     init() {
         super.init();
+    }
+
+    onResize() {
+        super.onResize();
+        this.elementsHolder.removeElement("ChangePositionButton");
+        this.elementsHolder.removeElement("RemoveObjectButton");
+        this.elementsHolder.addElement("ChangePositionButton", new ChangePositionButton(this.game, this.menu));
+        this.elementsHolder.addElement("RemoveObjectButton", new RemoveObjectButton(this.game, this.menu));
     }
 }
 
