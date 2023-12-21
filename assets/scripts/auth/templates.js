3161105f73ea80d2d43209032a216f29e82bae58
Eryczal <79263592+Eryczal@users.noreply.github.com>
2023-12-21 13:58:28 +0100

    Update Auth Templates
    Update login and register templates.

--- assets/scripts/auth/templates.js
+++ assets/scripts/auth/templates.js
@@ -1,45 +1,58 @@
 const loginTemplate = `
-    <div id="login-panel">
-        <form>
-            <h1>Logowanie</h1>
-            <label>
-                <span>Email: </span>
-                <input type="text" id="email">
-            </label>
-            <label>
-                <span>Hasło: </span>
-                <input type="password" id="password">
-            </label>
-            <button id="auth-button">Zaloguj</button>
-            <div class="small-text">
-                Nie masz konta? <span role="link" id="auth-link" tabindex="0">Zarejestruj się</span>
-            </div>
-        </form>
-    </div>
+  <div id="login-panel">
+    <form>
+      <h1>Logowanie</h1>
+
+      <label for="email">
+        <span>Email:</span>
+      </label>
+      <input type="text" id="email">
+
+      <label for="password">
+        <span>Hasło:</span>
+      </label>
+      <input type="password" id="password">
+
+      <hr>
+
+      <button id="auth-button">Zaloguj</button>
+
+      <div class="small-text">
+        Nie masz konta? <span role="link" id="auth-link" tabindex="0">Zarejestruj się</span>
+      </div>
+    </form>
+  </div>
 `;
 
 const registerTemplate = `
-    <div id="register-panel">
-        <form>
-            <h1>Rejestracja</h1>
-            <label>
-                <span>Email: </span>
-                <input type="text" id="email">
-            </label>
-            <label>
-                <span>Hasło: </span>
-                <input type="password" id="password">
-            </label>
-            <label>
-                <span>Powtórz hasło: </span>
-                <input type="password" id="repeat-password">
-            </label>
-            <button id="auth-button">Rejestruj</button>
-            <div class="small-text">
-                Masz już konto? <span role="link" id="auth-link" tabindex="0">Zaloguj się</span>
-            </div>
-        </form>
-    </div>
+  <div id="register-panel">
+    <form>
+      <h1>Rejestracja</h1>
+
+      <label for="email">
+        <span>Email: </span>
+      </label>
+      <input type="text" id="email">
+
+      <label for="password">
+        <span>Hasło: </span>
+      </label>
+      <input type="password" id="password">
+
+      <label for="repeat-password">
+        <span>Powtórz hasło: </span>
+      </label>
+      <input type="password" id="repeat-password">
+
+      <hr>
+
+      <button id="auth-button">Rejestruj</button>
+
+      <div class="small-text">
+        Masz już konto? <span role="link" id="auth-link" tabindex="0">Zaloguj się</span>
+      </div>
+    </form>
+  </div>
 `;
 
 export { loginTemplate, registerTemplate };
